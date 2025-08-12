5ab541f25adc6bb0c72a409f961956dac6f002a3
hardik-bhalgamiya <hardikbhalgamiya@gmail.com>
2025-08-12 23:51:14 +0530

    Update AppConstant.swift

--- VPracticalTask/VPracticalTask/AppConstant.swift
+++ VPracticalTask/VPracticalTask/AppConstant.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct AppConstant {
     
-    static let apiBaseUrl = "https://artwork.vasundharaapps.com/artwork_cache/api/AdvertiseNewApplications/17/com.fancy.fonts.style.keyboard.emojis.screen.number"
+    static let apiBaseUrl = ""
     
     static let appLinkPrefix = "https://play.google.com/store/apps/details?id="
     
